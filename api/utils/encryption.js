5201c502a9902bb7894c492c9c3f983249159ccb
Noel Luhanga <noeljayr01@gmail.com>
2024-10-21 14:53:04 +0200

    Update encryption.js

--- api/utils/encryption.js
+++ api/utils/encryption.js
@@ -2,8 +2,8 @@ const crypto = require('crypto');
 
 const encrypt = (text) => {
   const algorithm = 'aes-256-cbc';
-  const key = Buffer.from('376476efaac138b98fef1db0d836bda241cf7f0ba63cc733b5155014e2df6651', 'hex');  // Ensure key is 32 bytes
-  const iv = Buffer.from('816e0b8e3180b6c767b182296a1222c5', 'hex');  // Ensure IV is 16 bytes
+  const key = Buffer.from('', 'hex');  // Ensure key is 32 bytes
+  const iv = Buffer.from('', 'hex');  // Ensure IV is 16 bytes
 
   const cipher = crypto.createCipheriv(algorithm, key, iv);
   let encrypted = cipher.update(text, 'utf8', 'hex');
